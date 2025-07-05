d72218d0affed0c63c3f8ceeda674ce3c6b0ed85
Andreas Zogg <andreas.zogg@swisscom.com>
2025-07-05 19:59:12 +0200

    feat: add pinmapping header file for GPIO channel mapping and definitions

---
+++ src/pinmapping.h
@@ -0,0 +1,40 @@
+// pinmapping.h
+
+#ifndef _PINMAPPING_h
+#define _PINMAPPING_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+static uint8_t ChannelToGPIOMapping[16] = {
+	17, // O11
+	16, // O12
+	15, // O13
+	14, // O14
+	10, // O15
+	9,  // O16
+	8,  // O17
+	4,  // O18
+
+	42, // JP1
+	41, // JP2
+	40, // JP3
+};
+
+#define Input1         47
+#define Input2         48
+
+#define SD_CS         10 
+#define SPI_MOSI      11
+#define SPI_MISO      13
+#define SPI_SCK       12
+#define I2S_DOUT       7
+#define I2S_BCLK       5
+#define I2S_LRCLK      6
+#define LED_BUILTIN   47
+
+#endif
+
