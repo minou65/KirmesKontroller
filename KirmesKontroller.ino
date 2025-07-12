3a0ba4473d513e5a378f5385da5bc2c40f2f791d
Andreas Zogg <andreas.zogg@swisscom.com>
2025-07-12 14:14:20 +0200

    feat: enhance decoder initialization with accessory configuration and support for various modes

--- KirmesKontroller.ino
+++ KirmesKontroller.ino
@@ -4,6 +4,7 @@
 #include "src/version.h"
 #include "src/MotorControl.h"
 #include "src/SoundControl.h"
+#include "src/Blinkers.h"
 #include "src/pinmapping.h"
 #include "src/Vector.h"
 #include "src/accessories.h"
@@ -61,21 +62,99 @@ void kDecoderInit(void) {
 	decoder.Clear();
 	Vector<accessories*>().Swap(decoder);
 
-	// Initialize the decoder here if needed
-	
-	accessories* newAccessory = nullptr;
+	OutputGroup* outputgroup_ = &OutputGroup1;
+	while (outputgroup_ != nullptr) {
+		if (channel_ > 15) {
+			Serial.println("no more free channels!");
+			break;
+		}
 
-	uint8_t Mode_ = 203; // Default Mode for Motor accessory
-	uint16_t Address_ = 0;
+		if (outputgroup_->isActive()) {
+			uint8_t Mode_ = atoi(outputgroup_->_ModeValue);
+			uint8_t Count_ = atoi(outputgroup_->_NumberValue);
+			uint16_t Address_ = atoi(outputgroup_->_AddressValue);
+			uint8_t TimeOn_ = atoi(outputgroup_->_TimeOnValue);
+			uint8_t TimeOff_ = atoi(outputgroup_->_TimeOffValue);
+			uint8_t Multiplier_ = atoi(outputgroup_->_MultiplierValue); // Multiplikator
+			uint8_t TimeOnFade_ = atoi(outputgroup_->_TimeOnFadeValue);
+			uint8_t TimeOffFade_ = atoi(outputgroup_->_TimeOffFadeValue);
+			uint8_t Brightness_ = atoi(outputgroup_->_BrightnessValue);
 
-	newAccessory = new Motor(Address_, Mode_); // Create a new Motor accessory
+			uint16_t DayLightAddress_ = 0;
+			uint8_t DayBrightness_ = 255;
+			uint8_t NightBrightness_ = 150;
 
-	if (newAccessory != nullptr) {
-		Serial.print(F("    Mode: ")); Serial.println("Motor");
-		decoder.PushBack(newAccessory);
+			Serial.print(F("Values for channel ")); Serial.print(channel_); Serial.println(F(" preserved"));
+			Serial.print(F("    Channels used: ")); Serial.println(Count_);
+			Serial.print(F("    Address: ")); Serial.println(Address_);
+
+			// Einrichten des Ports
+			accessories* newAccessory = nullptr;
+			switch (Mode_) {
+			case 0:
+				newAccessory = new accessories(Address_, channel_);
+				channel_ += Count_;
+				break;
+			case 40: // einfacher Ausgang
+				newAccessory = new Ausgang(Address_, channel_);
+				channel_ += 1;
+				break;
+			case 50: // Blinker
+				newAccessory = new Blinker(Address_, channel_, Multiplier_ * TimeOff_, Multiplier_ * TimeOn_, TimeOnFade_, TimeOffFade_, Mode_);
+				channel_ += 1;
+				break;
+			case 51: // Wechselblinker
+				newAccessory = new Wechselblinker(Address_, channel_, Multiplier_ * TimeOff_, Multiplier_ * TimeOn_, TimeOnFade_, TimeOffFade_);
+				channel_ += 2;
+				break;
+			case 80: // TV
+				newAccessory = new Fernseher(Address_, channel_);
+				channel_ += 1;
+				break;
+			case 81: // Schweisslicht
+				newAccessory = new Schweissen(Address_, channel_, Multiplier_ * TimeOff_, Multiplier_ * TimeOn_);
+				channel_ += 3;
+				break;
+			case 82: // Feuer
+				newAccessory = new Feuer(Address_, channel_);
+				channel_ += 3;
+				break;
+			case 83: // Blitzlicht
+				newAccessory = new Blitzlicht(Address_, channel_, TimeOnFade_ * 1000, TimeOffFade_ * 1000, TimeOn_ * Multiplier_, TimeOff_ * Multiplier_);
+				channel_ += 1;
+				break;
+			case 203:// Motor
+				newAccessory = new Motor(Address_, Mode_); // Create a new Motor accessory
+				break;
+			}
+
+			if (newAccessory != nullptr) {
+				Serial.print(F("    Mode: ")); Serial.println(Mode_);
+
+				// Check the type of newAccessory
+				if (newAccessory->getType() == AccessoryType::LED) {
+					Ausgang* ledAccessory = static_cast<Ausgang*>(newAccessory);
+					// If the cast is successful, set the brightness
+					ledAccessory->SetMaxBrightness(Brightness_);
+				}
+				decoder.PushBack(newAccessory);
+			}
+		}
+		outputgroup_ = (OutputGroup*)outputgroup_->getNext();
 	}
 
-	Serial.println("kDecoder Initialized");
+	ServoGroup* servogroup_ = &ServoGroup1;
+	while (servogroup_ != nullptr) {
+		if (channel_ > 15) {
+			Serial.println("no more free channels!");
+			break;
+		}
+
+		if (servogroup_->isActive()) {
+
+		}
+		servogroup_ = (ServoGroup*)servogroup_->getNext();
+	}
 }
 
 void setup() {
