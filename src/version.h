f5c8da45a0955d75d8d5318053bdd4d769e40948
Andy <azogg65@bluewin.ch>
2025-07-14 17:51:07 +0200

    feat: enhance Motor accessory initialization with time-based activation

--- src/version.h
+++ src/version.h
@@ -1,10 +1,10 @@
 #define VERSION_MAJOR 0
-#define VERSION_MINOR 3
+#define VERSION_MINOR 5
 #define VERSION_PATCH 0
 
 // Automatically generated build version. Do not modify.
-#define VERSION_BUILD 55
+#define VERSION_BUILD 71
 #define VERSION_DATE "2025-07-14"
-#define VERSION_TIME "15:33:55"
-#define VERSION "0.3.0.55"
-#define VERSION_STR "0.3.0.55 (2025-07-14 15:33:55)"
+#define VERSION_TIME "17:38:07"
+#define VERSION "0.5.0.71"
+#define VERSION_STR "0.5.0.71 (2025-07-14 17:38:07)"
