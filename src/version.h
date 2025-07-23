812dcbc717b7f5a2e707216fd1ef142dd7c0ff1b
Andy <azogg65@bluewin.ch>
2025-07-23 06:08:39 +0200

    chore: update version to 0.9.0.236 with new build date and time

--- src/version.h
+++ src/version.h
@@ -3,8 +3,8 @@
 #define VERSION_PATCH 0
 
 // Automatically generated build version. Do not modify.
-#define VERSION_BUILD 166
-#define VERSION_DATE "2025-07-21"
-#define VERSION_TIME "06:36:03"
-#define VERSION "0.9.0.166"
-#define VERSION_STR "0.9.0.166 (2025-07-21 06:36:03)"
+#define VERSION_BUILD 236
+#define VERSION_DATE "2025-07-22"
+#define VERSION_TIME "19:58:19"
+#define VERSION "0.9.0.236"
+#define VERSION_STR "0.9.0.236 (2025-07-22 19:58:19)"
