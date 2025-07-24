ff4e4406ce6efedb27affbb6ea53beb8a7a15853
Andy <azogg65@bluewin.ch>
2025-07-24 10:36:54 +0200

    feat: update version to 0.9.0.259 with new build date and time

--- src/version.h
+++ src/version.h
@@ -3,8 +3,8 @@
 #define VERSION_PATCH 0
 
 // Automatically generated build version. Do not modify.
-#define VERSION_BUILD 84
-#define VERSION_DATE "2025-07-17"
-#define VERSION_TIME "10:36:39"
-#define VERSION "0.6.0.84"
-#define VERSION_STR "0.6.0.84 (2025-07-17 10:36:39)"
+#define VERSION_BUILD 259
+#define VERSION_DATE "2025-07-24"
+#define VERSION_TIME "09:24:42"
+#define VERSION "0.9.0.259"
+#define VERSION_STR "0.9.0.259 (2025-07-24 09:24:42)"
