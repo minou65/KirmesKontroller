e56fde3589611d513f15c748ee17b03f8d5877e8
Andy <azogg65@bluewin.ch>
2025-07-26 17:41:00 +0200

    fix: refactor MotorControl class to improve constructor parameters and enhance attach/detach functionality

--- src/MotorControl.cpp
+++ src/MotorControl.cpp
@@ -11,45 +11,88 @@
 #include "MotorControl.h"
 
 
-MotorControl::MotorControl(const uint8_t Channel) :
+MotorControl::MotorControl(const uint8_t pin) :
 	_maxSpeed(255),
 	_Speed(0),
-	LED(Channel, PWM_Set_Off) {
+	_GPIO(pin) {
 
 	Serial.println("MotorControl::MotorControl");
+	_Frequency = 5000; // Default frequency
+	_Resolution = 8; // Default resolution
+	Serial.print("    GPIO: "); Serial.println(_GPIO);
+	attach(_GPIO);
 	off();
 }
 
-MotorControl::MotorControl(const uint8_t Channel, uint8_t Speed) :
-	MotorControl(Channel) {
+MotorControl::MotorControl(const uint8_t pin, uint8_t Speed) :
+	MotorControl(pin) {
 	setMaxSpeed(Speed);
 }
 
-MotorControl::MotorControl(const uint8_t Channel, const uint16_t Frequency, const uint8_t Resolution) :
-	MotorControl(Channel) {
-	setFrequency(Frequency);
-	setPWM(Resolution);
+MotorControl::MotorControl(const uint8_t pin, const uint16_t frequency, const uint8_t resolution) :
+	MotorControl(pin) {
+	setFrequency(frequency);
+	setPWM(resolution);
 }
 
 MotorControl::~MotorControl() {
 	Serial.println("MotorControl::~MotorControl");
+	detach();
 	off();
 }
 
+bool MotorControl::attach(uint8_t pin) {
+	if (_isAttached) {
+		Serial.println("MotorControl::attach: already attached");
+		return false;
+	}
+	_isAttached = ledcAttach(pin, _Frequency, _Resolution);
+	if (_isAttached) {
+		Serial.println("MotorControl::attach: success");
+	} else {
+		Serial.println("MotorControl::attach: failed");
+	}
+	return _isAttached;
+};
+
+void MotorControl::detach() {
+	if (!_isAttached) {
+		Serial.println("MotorControl::detach: not attached");
+		return;
+	}
+	Serial.println("MotorControl::detach");
+	ledcDetach(_GPIO);
+	_isAttached = false;
+};
+
+void MotorControl::write(uint16_t value) {
+	if (!_isAttached) {
+		Serial.println("MotorControl::write: not attached");
+		return;
+	}
+	if (value > _maxSpeed) {
+		value = _maxSpeed;
+	}
+	ledcWrite(_GPIO, value);
+	_Speed = value; // Update the current speed
+	Serial.print("MotorControl::write: Speed set to ");
+	Serial.println(_Speed);
+
+};
+
 void MotorControl::process() {
-	LED::process();
 }
 
 void MotorControl::on(){
 	Serial.println("MotorControl::on");
 	_IsActive = true;
-	ledcWrite(_Channel, _Speed);
+	write(_Speed);
 }
 
 void MotorControl::off(){
 	Serial.println("MotorControl::off");
 	_IsActive = false;
-	ledcWrite(_Channel, PWM_Set_Off);
+	write(PWM_Set_Off);
 }
 
 void MotorControl::toggle(){
@@ -74,17 +117,19 @@ void MotorControl::setMaxSpeed(int speed) {
 }
 
 void MotorControl::setPWM(int pwm){
-	_PWMResolution = pwm;
-	ledcDetach(_GPIO); // Detach the pin before changing PWM settings
-	ledcAttachChannel(_GPIO, _PWMFrequency, _PWMResolution, _Channel); // Attach the pin with the new settings
-	_Speed = map(_Speed, 0, 255, 0, (1 << _PWMResolution) - 1); // Map speed to PWM resolution
-	ledcWrite(_Channel, _Speed); // Update the PWM signal with the new resolution
+	_Resolution = pwm;
+	detach(); // Detach the pin before changing PWM settings
+
+	////ledcDetach(_GPIO); // Detach the pin before changing PWM settings
+	////ledcAttachChannel(_GPIO, _PWMFrequency, _PWMResolution, _Channel); // Attach the pin with the new settings
+	//_Speed = map(_Speed, 0, 255, 0, (1 << _PWMResolution) - 1); // Map speed to PWM resolution
+	//write(_Speed); // Update the PWM signal with the new resolution
 
 }
 
 void MotorControl::setFrequency(int frequency){
-	_PWMFrequency = frequency;
-	ledcDetach(_GPIO); // Detach the pin before changing PWM settings
-	ledcAttachChannel(_GPIO, _PWMFrequency, _PWMResolution, _Channel); // Attach the pin with the new settings
-	ledcWrite(_Channel, _Speed); // Update the PWM signal with the new frequency
+	//_PWMFrequency = frequency;
+	////ledcDetach(_GPIO); // Detach the pin before changing PWM settings
+	////ledcAttachChannel(_GPIO, _PWMFrequency, _PWMResolution, _Channel); // Attach the pin with the new settings
+	//write(_Speed); // Update the PWM signal with the new frequency
 }
--- src/MotorControl.h
+++ src/MotorControl.h
@@ -11,13 +11,17 @@
 
 #include "LEDControl.h"
 
-class MotorControl : public LED {
+class MotorControl {
 
 public:
-    MotorControl(const uint8_t Channel);
-    MotorControl(const uint8_t Channel, uint8_t Speed);
-    MotorControl(const uint8_t Channel, const uint16_t Frequency, const uint8_t Resolution);
+    MotorControl(const uint8_t pin);
+    MotorControl(const uint8_t pin, uint8_t Speed);
+    MotorControl(const uint8_t pin, const uint16_t frequency, const uint8_t resolution);
     ~MotorControl();
+
+	bool attach(uint8_t pin);
+	void detach();
+	void write(uint16_t value);
 
     void process();
 
@@ -25,14 +29,24 @@ public:
     void off();
     void toggle();
 
+	bool isOn() const { return _IsActive; }
+
     void setMaxSpeed(int speed);
     void setPWM(int pwm);
     void setFrequency(int frequency);
 
 private:
+	uint8_t _GPIO;
+	bool _IsActive;
+
     int _Speed; // 0-255 for PWM control
     int _maxSpeed;
     bool _isOn = false;
+
+    bool _isAttached = false;
+
+    uint16_t _Frequency;
+    uint16_t _Resolution;
 };
 
 
